84c39fe620501234eb2625a9d6449d10496a5fd1
Juan Mosquella <jmosquella11@gmail.com>
2022-05-03 02:04:56 -0300

    Termiando js de la pokedex

--- app.js
+++ app.js
@@ -2,16 +2,14 @@ const container = document.querySelector('.container-photo');
 const containerInfo = document.querySelector('.container-info-pokemon')
 const input = document.getElementById('input');
 const div = document.querySelector('.card');
-const containerStats = document.querySelector('.stats')
-const containerTypes = document.querySelector('.types')
-
-
+const containerTitle = document.querySelector('.title');
+const containerStats = document.querySelector('.stats');
+const containerTypes = document.querySelector('.types');
 
 document.addEventListener('click', (e) => {
 
-    if(e.target.matches('.btn')){
-        e.preventDefault();
-        container.innerHTML = "";
+    if(e.target.matches('.circle')){
+        e.preventDefault();       
         searchPokemon()
         
     }
@@ -21,12 +19,13 @@ document.addEventListener('click', (e) => {
 const searchPokemon = async () => {        
    
     try{
-        const url = await fetch(`https://pokeapi.co/api/v2/pokemon/${input.value}`)
-        
+        const url = await fetch(`https://pokeapi.co/api/v2/pokemon/${input.value}`)        
         const data = await url.json();
-        
+       
         printCards(data);
-        printStats(data)
+        printTitle(data)
+        printStats(data);
+        printTypes(data.types[0], data.types[1] )
 
     }
     catch{
@@ -37,6 +36,15 @@ const searchPokemon = async () => {
     }
 }
 
+
+const printTitle = (data) => {
+    containerTitle.innerHTML = "";
+    const title = document.createElement('h3');      
+    title.innerHTML = data.name;
+    containerTitle.appendChild(title)  
+
+}
+
 const printCards = (data) => { 
     
     div.innerHTML = "";   
@@ -44,21 +52,27 @@ const printCards = (data) => {
     img.setAttribute('src', data.sprites.front_default)
     div.appendChild(img);
     div.style.backgroundColor = '#FFF';    
-    container.appendChild(div)   
+    container.appendChild(div)
+}
+
+const printTypes = (data1, data2) => {
+    containerTypes.innerHTML = "";
+    const typePrimary = document.createElement('div');
+     typePrimary.className = 'type-primary';
+     typePrimary.innerHTML = data1.type.name;
+     const typeSecondary = document.createElement('div');
+     typeSecondary.className = 'type-secondary';
+     typeSecondary.innerHTML = data2.type.name;     
+     containerTypes.appendChild(typePrimary);
+     containerTypes.appendChild(typeSecondary);
+    
+     
+     
 
 }
 
-const printStats = (data) => {
-     const typePrimary = document.createElement('div');
-     typePrimary.className = 'type-primary';
-     typePrimary.innerHTML = data.types;
-     const typeSecondary = document.createElement('div');
-     typeSecondary.className = 'type-secondary';
-     containerTypes.appendChild(typePrimary, typeSecondary);
-    
-    const title = document.createElement('h3');    
-    title.innerHTML = data.name;
-    containerInfo.insertBefore(title, containerTypes);
+const printStats = (data) => {   
+     containerStats.innerHTML = "";    
     Object.values(data.stats).forEach(stat => {
         const li = document.createElement('li');
         const span = document.createElement('span');
